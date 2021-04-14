31f94678996bd53e00fc1eff4f58a6d0eee519a2
andmos21 <andmos4@gmail.com>
2021-04-14 15:36:14 +0200

    Updating some stuff

--- Warhammer.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Warhammer.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Warhammer, "Warhammer" );
